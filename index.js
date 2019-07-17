ac07aa799b47bba916b6e38e2638d13533795179
Volchar <volchar@gmail.com>
2019-07-17 15:29:58 +0300

    Add new style par3

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 document.getElementById("par1").innerHTML = "Goodbye world!";
-document.getElementById("par2").style.color = "blue";
-document.getElementById("par1").style.color = "red";
+document.getElementById("par1").style.color = "blue";
+document.getElementById("par2").style.color = "red";
+document.getElementById("par3").style.color = "green";
 document.getElementById("par1").style.fontSize = "30px";
 document.getElementById("par2").style.fontSize = "20px";
+document.getElementById("par3").style.fontSize = "24px";
 document.getElementById("par1").style.fontFamily = "Verdana";
 document.getElementById("par2").style.fontFamily = "Verdana";
+document.getElementById("par3").style.fontFamily = "Verdana";
