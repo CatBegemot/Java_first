5f3e924f69bad4a49d1b8c0ed171d9064ab6a2d6
Volchar <volchar@gmail.com>
2019-07-17 15:07:16 +0300

    Add .js, change .htl - styles to .js

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("par1").innerHTML = "Goodbye world!";
+document.getElementById("par2").style.color = "blue";
+document.getElementById("par2").style.color = "red";
+document.getElementById("par1").style.fontSize = "30px";
+document.getElementById("par2").style.fontSize = "20px";
+document.getElementById("par1").style.fontFamily = "Verdana";
+document.getElementById("par2").style.fontFamily = "Verdana";
