f3e2cc503ed6d596d43c1ccea494b6af47a3faa9
Volchar <volchar@gmail.com>
2019-07-17 15:26:27 +0300

    Change style color for par1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.getElementById("par1").innerHTML = "Goodbye world!";
 document.getElementById("par2").style.color = "blue";
-document.getElementById("par2").style.color = "red";
+document.getElementById("par1").style.color = "red";
 document.getElementById("par1").style.fontSize = "30px";
 document.getElementById("par2").style.fontSize = "20px";
 document.getElementById("par1").style.fontFamily = "Verdana";
