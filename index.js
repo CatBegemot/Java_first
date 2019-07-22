725b90b7c2608dfac3221a4360792850e07bcb05
Volchar <volchar@gmail.com>
2019-07-23 01:23:56 +0300

    Add some beauty

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 var div1 = document.createElement("div1");
 document.body.appendChild(div1);
+
 var div2 = document.createElement("div2");
 document.body.appendChild(div2);
+
 var par1 = document.createElement("p");
 document.body.appendChild(par1);
-var pict = document.createElement("img");
-document.body.appendChild(pict);
 div2.appendChild(par1);
+par1.innerHTML = "Goodbye world!";
+
 var link1 = document.createElement("a");
 document.body.appendChild(link1);
-par1.innerHTML = "Goodbye world!";
+
+var pict = document.createElement("img");
+document.body.appendChild(pict);
 pict.src = "/picts/smBlackCat.jpg" ;
+
+
