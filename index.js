97be30ff8d4d29575ba1099e5f291e2a59cb3e9a
Volchar <volchar@gmail.com>
2019-07-22 23:39:07 +0300

    Create 2 div & 1 par1, change propeties

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 var div = document.createElement("div");
 document.body.appendChild(div);
-var par1 = document.createElement("p")
+var par1 = document.createElement("p");
 div.appendChild(par1);
-div1.id = "div1"
-//document.getElementById("par1").innerHTML = "Goodbye world!";//
-// document.getElementById("par1").style.color = "blue";
-// document.getElementById("par2").style.color = "red";
-// document.getElementById("par3").style.color = "green";
-// document.getElementById("par1").style.fontSize = "30px";
-// document.getElementById("par2").style.fontSize = "20px";
-// document.getElementById("par3").style.fontSize = "24px";
-// document.getElementById("par1").style.fontFamily = "Verdana";
-// document.getElementById("par2").style.fontFamily = "Verdana";
-// document.getElementById("par3").style.fontFamily = "Verdana";
+div1.id = "div1";
