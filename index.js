e20d26eb19192f5c3cff350fd30da22f8276a46d
Volchar <volchar@gmail.com>
2019-07-22 23:47:14 +0300

    Create div2

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var div = document.createElement("div");
 document.body.appendChild(div);
+var div1 = document.createElement("div1");
+document.body.appendChild(div1);
+var div2 = document.createElement("div2");
+document.body.appendChild(div2);
 var par1 = document.createElement("p");
+document.body.appendChild(par1);
+
 div.appendChild(par1);
-div1.id = "div1";
+div1.appendChild(par1);
