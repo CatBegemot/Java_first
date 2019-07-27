277243b603f37dff7bccd907e40e16d9236b4558
Volchar <volchar@gmail.com>
2019-07-28 01:38:07 +0300

    Change home and contacts html&js

---
+++ contacts.js
@@ -0,0 +1,20 @@
+var pict_home = document.createElement("img");
+document.body.appendChild(pict_home);
+pict_home.src = "../My_web/img/home.jpg";
+var link_home = document.createElement("A");
+document.body.appendChild(link_home);
+link_home.href="home.html";
+
+var pict_about = document.createElement("img");
+document.body.appendChild(pict_about);
+pict_about.src = "../My_web/img/about.jpg";
+var link_about = document.createElement("A");
+document.body.appendChild(link_about);
+link_about.href="about.html";
+
+var pict_contacts = document.createElement("img");
+document.body.appendChild(pict_contacts);
+pict_contacts.src = "../My_web/img/contacts_1.jpg";
+var link_contacts = document.createElement("A");
+document.body.appendChild(link_contacts);
+link_contacts.href="contacts.html";
