b0bced49f3ccb75f88d585e2f1a1f33f1961d067
Volchar <volchar@gmail.com>
2019-07-28 01:31:06 +0300

    Change about.js with picts

--- about.js
+++ about.js
@@ -1,106 +1,48 @@
-var link1 = document.createElement("a");
-document.body.appendChild(link1);
-link1.href="../typescript-tutorial/about.html";
-
 var pict_home = document.createElement("img");
 document.body.appendChild(pict_home);
 pict_home.src = "../My_web/img/home.jpg";
-var link_home = document.createElement("a");
+var link_home = document.createElement("A");
 document.body.appendChild(link_home);
-link_home.href="../home.html";
+link_home.href="home.html";
 
 var pict_about = document.createElement("img");
 document.body.appendChild(pict_about);
 pict_about.src = "../My_web/img/about_1.jpg";
-var link_about = document.createElement("a");
+var link_about = document.createElement("A");
 document.body.appendChild(link_about);
-link_about.href="../about.html";
+link_about.href="about.html";
 
 var pict_contacts = document.createElement("img");
 document.body.appendChild(pict_contacts);
 pict_contacts.src = "../My_web/img/contacts.jpg";
-var link_contacts = document.createElement("a");
+var link_contacts = document.createElement("A");
 document.body.appendChild(link_contacts);
-link_contacts.href="../contacts.html";
+link_contacts.href="contacts.html";
 
-var pict1 = document.createElement("img1");
+var pict1 = document.createElement("img");
 document.body.appendChild(pict1);
+pict1.style.position = "absolute";
+pict1.style.top = "100px"; 
+pict1.style.left = "10px";
 pict1.src = "../My_web/img/pic_1.jpg";
-img1.position = "absolute";
-img1.top = "0"; 
-img1.left = "0";
 
-/* img2.className = "menuHome"; 
-img2.position = "absolute";
-img2.top = "0"; 
-img2.left = "150px";
+var pict2 = document.createElement("img");
+document.body.appendChild(pict2);
+pict2.style.position = "absolute";
+pict2.style.top = "100px"; 
+pict2.style.left = "160px";
+pict2.src = "../My_web/img/pic_2.jpg";
 
-img3.className = "menuHome"; 
-img3.position = "absolute";
-img3.top = "150px"; 
-img3.left = "0";
+var pict3 = document.createElement("img");
+document.body.appendChild(pict3);
+pict3.style.position = "absolute";
+pict3.style.top = "250px"; 
+pict3.style.left = "10px";
+pict3.src = "../My_web/img/pic_3.jpg";
 
-img4.className = "menuHome"; 
-img4.position = "absolute";
-img4.top = "150px"; 
-img4.left = "150px";
-
-
-    /*
-class menuAbout {
-    position = "relative";
-    zindex = "0";
-    top = "20px"; 
-    left = "70px";
-    }
-class menuContacts {
-    position = "relative";
-    zindex = "0";
-    top = "20px"; 
-    left = "120px";
-    }
-class divcomm {
-      position = "relative";
-      backgroundcolor = "#ffffff";
-      width = "300px";
-      height = "300px";
-      border = "1px solid red";
-      top = "100px";
-      marginleft = "auto";
-      marginright = "auto";
-      z-index = "0";
-    }
-class imgall {
-      zindex = "0";
-      width = "150px";
-      height = "150px";
-    }
-
-    img.className = "menuAbout"; 
-
-img.className = "menuContacts"; 
-
-link1.href="../typescript-tutorial/home.html";
-link1.style.color = "red";
-link1.style.fontSize = "20px";
-link1.style.fontFamily = "Verdana";
-link1.innerHTML = "Warior";
-
-<a href="../typescript-tutorial/index.html">
-<img ="menuHome" src="../My_web/img/home.jpg" alt ="About">
-</a>
-<a href="about.html">
-<img id="menuAbout" src="../My_web/img/about_1.jpg" alt ="About">
-</a>
-<a href="contacts.html">
-<img id="menuContacts" src="../My_web/img/contacts.jpg" alt ="Contacts">
-</a>
-<div id="divcomm">
-<div class="imgall">
- <img id="img1" src="../My_web/img/pic_1.jpg" alt="Самурай"></img>
- <img id="img2" src="../My_web/img/pic_2.jpg" alt="Самурай"></img>
- <img id="img3" src="../My_web/img/pic_3.jpg" alt="Самурай"></img>
- <img id="img4" src="../My_web/img/pic_4.jpg" alt="Самурай"></img>
-</div>
-</div>
-*/
+var pict4 = document.createElement("img");
+document.body.appendChild(pict4);
+pict4.style.position = "absolute";
+pict4.style.top = "250px"; 
+pict4.style.left = "160px";
+pict4.src = "../My_web/img/pic_4.jpg";
